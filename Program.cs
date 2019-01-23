5156dbc97177fa05b20568bc8ee4b886943a00d6
msverko <m.sverko@gmail.com>
2019-01-23 13:54:32 +0100

    First push

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ScreenShot
+{
+    class Program
+    {
+        static DirectoryInfo di;
+        static void Main(string[] args)
+        {
+            Magician.DisappearConsole();
+
+            args = new string[] { "y" };
+
+            if (args[0] == "y")
+            {
+
+                di = new DirectoryInfo("D:\\test");
+                if (!di.Exists) { di.Create(); }
+
+                PrintScreen ps = new PrintScreen();
+                ps.CaptureScreenToFile(di + "\\screen.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+
+
+        }
+    }
+}
